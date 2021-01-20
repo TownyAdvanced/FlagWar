305fab6747435acd28bd73c00b6af60631a6771a
FlagCourier <flagcourier@pm.me>
2021-01-20 13:04:04 -0600

    Remove Non-FlagWar Files
    Removes all Towny files not related directly to FlagWar
    
    The following resources are retained for reference/editing purposes:
    - english.yml
    - plugin.yml
    
    The following files will be replaced for FlagWar:
    - License.md >> LICENSE - Transitioning to a more permissive license.

--- src/com/palmergames/annotations/Unmodifiable.java
+++
@@ -1,8 +0,0 @@
-package com.palmergames.annotations;
-
-/**
- * Marks whether a method returns a collection that cannot be changed.
- * Specifically, elements cannot be added or removed.
- */
-public @interface Unmodifiable {
-}
--- src/com/palmergames/bukkit/config/migration/MigrationType.java
+++
@@ -1,8 +0,0 @@
-package com.palmergames.bukkit.config.migration;
-
-public enum MigrationType {
-	OVERWRITE,
-	APPEND,
-	NATION_LEVEL_ADD,
-	TOWN_LEVEL_ADD
-}
