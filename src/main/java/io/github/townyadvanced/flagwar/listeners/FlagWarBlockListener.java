94708ad67c17ee4a614b85d52af44c447ab7add7
FlagCourier <flagcourier@pm.me>
2021-04-16 00:32:57 -0500

    FlagWarBlockListener CheckStyle Compliance; New private Method
    Makes FlagWarBlockListener compliant with the maven checkstyle profile (Modified Sun Style)
    
    Adds private wrapper method for attempting to call the FlagWar#callAttackCellEvent(Towny, Player, Block, Cancellable) method.

--- src/main/java/io/github/townyadvanced/flagwar/listeners/FlagWarBlockListener.java
+++ src/main/java/io/github/townyadvanced/flagwar/listeners/FlagWarBlockListener.java
@@ -17,6 +17,7 @@
 
 package io.github.townyadvanced.flagwar.listeners;
 
+import com.palmergames.bukkit.towny.event.actions.TownyActionEvent;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -37,68 +38,122 @@ import io.github.townyadvanced.flagwar.FlagWar;
 import io.github.townyadvanced.flagwar.config.FlagWarConfig;
 
 public class FlagWarBlockListener implements Listener {
-
+    /** Retains the {@link Towny} instance, after construction.  */
     private Towny towny;
 
+    /**
+     * Constructs the FlagWarBlockListener, setting {@link #towny}.
+     *
+     * @param flagWar The FlagWar instance.
+     */
     public FlagWarBlockListener(final FlagWar flagWar) {
         if (flagWar.getServer().getPluginManager().getPlugin("Towny") != null) {
             this.towny = Towny.getPlugin();
         }
     }
 
+    /**
+     * Check if the {@link Player} from {@link TownyBuildEvent#getPlayer()} is attempting to build inside enemy lands,
+     * and if so, {@link #tryCallCellAttack(TownyActionEvent, Player, Block, WorldCoord)}.
+     *
+     * @param townyBuildEvent the {@link TownyBuildEvent}.
+     */
     @EventHandler (priority = EventPriority.HIGH)
     @SuppressWarnings("unused")
-    public void onFlagWarFlagPlace(final TownyBuildEvent event) {
-        if (event.getTownBlock() == null) {
+    public void onFlagWarFlagPlace(final TownyBuildEvent townyBuildEvent) {
+        if (townyBuildEvent.getTownBlock() == null
+            || !FlagWarConfig.isAllowingAttacks()
+            || !townyBuildEvent.getMaterial().equals(FlagWarConfig.getFlagBaseMaterial())) {
+
             return;
         }
 
-        if (!(FlagWarConfig.isAllowingAttacks() && event.getMaterial() == FlagWarConfig.getFlagBaseMaterial())) {
-            return;
-        }
-
-        Player player = event.getPlayer();
-        Block block = player.getWorld().getBlockAt(event.getLocation());
+        Player player = townyBuildEvent.getPlayer();
+        Block block = player.getWorld().getBlockAt(townyBuildEvent.getLocation());
         WorldCoord worldCoord = new WorldCoord(block.getWorld().getName(), Coord.parseCoord(block));
 
-        if (towny.getCache(player).getStatus() == TownBlockStatus.ENEMY) {
-            try {
-                if (FlagWar.callAttackCellEvent(towny, player, block, worldCoord)) {
-                    event.setCancelled(false);
-                }
-            } catch (TownyException e) {
-                event.setMessage(e.getMessage());
-            }
+        if (towny.getCache(player).getStatus().equals(TownBlockStatus.ENEMY)) {
+            tryCallCellAttack(townyBuildEvent, player, block, worldCoord);
         }
     }
 
+    /**
+     * Runs {@link FlagWar#checkBlock(Player, Block, org.bukkit.event.Cancellable)} using the event's {@link Player},
+     * {@link Block}, and the {@link BlockBreakEvent} itself.
+     *
+     * @param blockBreakEvent the {@link BlockBreakEvent}.
+     */
     @EventHandler(priority = EventPriority.LOWEST)
     @SuppressWarnings("unused")
-    public void onBlockBreak(final BlockBreakEvent event) {
-        FlagWar.checkBlock(event.getPlayer(), event.getBlock(), event);
+    public void onBlockBreak(final BlockBreakEvent blockBreakEvent) {
+        FlagWar.checkBlock(blockBreakEvent.getPlayer(), blockBreakEvent.getBlock(), blockBreakEvent);
     }
 
+    /**
+     * Runs {@link FlagWar#checkBlock(Player, Block, org.bukkit.event.Cancellable)} using the event's {@link Player},
+     * {@link Block}, and the {@link BlockBurnEvent} itself.
+     *
+     * @param blockBurnEvent the {@link BlockBurnEvent}.
+     */
     @EventHandler(priority = EventPriority.LOWEST)
     @SuppressWarnings("unused")
-    public void onBlockBurn(final BlockBurnEvent event) {
-        FlagWar.checkBlock(null, event.getBlock(), event);
+    public void onBlockBurn(final BlockBurnEvent blockBurnEvent) {
+        FlagWar.checkBlock(null, blockBurnEvent.getBlock(), blockBurnEvent);
     }
 
+
+    /**
+     * Iteratively runs over {@link FlagWar#checkBlock(Player, Block, org.bukkit.event.Cancellable)} using the event's
+     * {@link Player}, {@link Block} ({@link BlockPistonExtendEvent#getBlocks()}), and the
+     * {@link BlockPistonExtendEvent} itself.
+     *
+     * @param blockPistonExtendEvent the {@link BlockPistonExtendEvent}.
+     */
     @EventHandler(priority = EventPriority.LOWEST)
     @SuppressWarnings("unused")
-    public void onBlockPistonExtend(final BlockPistonExtendEvent event) {
-        for (Block block : event.getBlocks()) {
-            FlagWar.checkBlock(null, block, event);
+    public void onBlockPistonExtend(final BlockPistonExtendEvent blockPistonExtendEvent) {
+        for (Block block : blockPistonExtendEvent.getBlocks()) {
+            FlagWar.checkBlock(null, block, blockPistonExtendEvent);
         }
     }
 
+    /**
+     * Iteratively runs over {@link FlagWar#checkBlock(Player, Block, org.bukkit.event.Cancellable)} using the event's
+     * {@link Player}, {@link Block} ({@link BlockPistonRetractEvent#getBlocks()}), and the
+     * {@link BlockPistonRetractEvent} itself.
+     *
+     * Fails fast if {@link BlockPistonRetractEvent#isSticky()} is false.
+     *
+     * @param blockPistonRetractEvent the {@link BlockPistonRetractEvent}.
+     */
     @EventHandler(priority = EventPriority.LOWEST)
     @SuppressWarnings("unused")
-    public void onBlockPistonRetract(final BlockPistonRetractEvent event) {
-        if (event.isSticky()) {
-            for (Block block : event.getBlocks()) {
-                FlagWar.checkBlock(null, block, event);
+    public void onBlockPistonRetract(final BlockPistonRetractEvent blockPistonRetractEvent) {
+        if (!blockPistonRetractEvent.isSticky()) {
+            return;
+        }
+        for (Block block : blockPistonRetractEvent.getBlocks()) {
+            FlagWar.checkBlock(null, block, blockPistonRetractEvent);
+        }
+    }
+
+    /**
+     * Wrapper for {@link TownyActionEvent} methods needing to run the
+     * {@link FlagWar#callAttackCellEvent(Towny, Player, Block, WorldCoord)} method and, if it would return true,
+     * {@link TownyActionEvent#setCancelled(boolean)} to {@link Boolean#FALSE}.
+     *
+     * @param event the calling {@link TownyActionEvent}
+     * @param p the {@link Player}, typically result of {@link TownyActionEvent#getPlayer()}, being passed along.
+     * @param b the {@link Block} being passed along.
+     * @param wC the {@link WorldCoord} being passed along.
+     */
+    private void tryCallCellAttack(final TownyActionEvent event, final Player p, final Block b, final WorldCoord wC) {
+        try {
+            if (FlagWar.callAttackCellEvent(towny, p, b, wC)) {
+                event.setCancelled(false);
             }
+        } catch (TownyException townyException) {
+            event.setMessage(townyException.getMessage());
         }
     }
 }
