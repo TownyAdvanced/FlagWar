a2fa2c43283982336785c531aa27779e477cc214
LlmDl <LlmDlio@gmail.com>
2025-11-08 19:42:48 -0600

    Add /ta reload flagwar (#409)
    * Add /ta reload flagwar
    
    Command that reloads FW's config when ran.
    
    Closes #396
    
    * Missed word in javadoc.
    
    * Fix checkstyle and javadoc errors

---
+++ src/main/java/io/github/townyadvanced/flagwar/command/TownyAdminReloadAddon.java
@@ -0,0 +1,54 @@
+package io.github.townyadvanced.flagwar.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+
+import com.palmergames.bukkit.towny.TownyCommandAddonAPI;
+import com.palmergames.bukkit.towny.TownyUniverse;
+import com.palmergames.bukkit.towny.TownyCommandAddonAPI.CommandType;
+import com.palmergames.bukkit.towny.command.BaseCommand;
+import com.palmergames.bukkit.towny.object.AddonCommand;
+import io.github.townyadvanced.flagwar.FlagWar;
+import io.github.townyadvanced.flagwar.i18n.Translate;
+import io.github.townyadvanced.flagwar.util.Messaging;
+
+public class TownyAdminReloadAddon extends BaseCommand implements TabExecutor {
+
+    /**
+     * Method which will register the flagwar subcommand in Towny's /ta reload.
+     */
+    public TownyAdminReloadAddon() {
+        AddonCommand townyAdminReloadCommand = new AddonCommand(CommandType.TOWNYADMIN_RELOAD, "flagwar", this);
+        TownyCommandAddonAPI.addSubCommand(townyAdminReloadCommand);
+    }
+
+    /**
+     * onCommand class required by TabExecutor.
+     */
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command command, final String label,
+            final String[] args) {
+        parseAdminReloadCommand(args, sender);
+        return true;
+    }
+
+    private void parseAdminReloadCommand(final String[] args, final CommandSender sender) {
+        if (sender instanceof Player player) {
+            if (!TownyUniverse.getInstance().getPermissionSource().isTownyAdmin(player)) {
+                Messaging.send(player, Translate.fromPrefixed("error.command.disabled"));
+                return;
+            }
+        }
+
+        // Load config.yml
+        if (!FlagWar.getFlagWar().loadConfig()) {
+            Messaging.send(sender, Translate.fromPrefixed("error.invalid.config"));
+            return;
+        }
+
+        Messaging.send(sender, Translate.fromPrefixed("message.flag.war.config.has.been.reloaded"));
+    }
+
+}
